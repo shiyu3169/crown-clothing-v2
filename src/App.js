e0e15dbf22a1aa598d060336e3c22fe159494dd6
Shiyu Wang <shiyu3169@gmail.com>
2021-09-17 00:02:56 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Route } from "react-router"
 import "./App.css"
 import Homepage from "./pages/homepage/Homepage"
+import { ROUTE_NAMES } from "./constants/routes"
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact name={ROUTE_NAMES.HOMEPAGE} path="/" component={Homepage} />
     </div>
   )
 }
