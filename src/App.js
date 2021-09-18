f6bbbcd8c6ea6084c1c5a4825475c39953276b1f
Shiyu Wang <shiyu3169@gmail.com>
2021-09-17 22:31:53 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { Route, Switch } from "react-router"
 import "./App.css"
 import Homepage from "./pages/homepage/Homepage"
-import { ROUTE_NAMES } from "./constants/routes"
 import Shop from "./pages/shop/Shop"
+import Header from "./components/header/Header"
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route
-          exact
-          name={ROUTE_NAMES.HOMEPAGE}
-          path="/"
-          component={Homepage}
-        />
-        <Route name={ROUTE_NAMES.SHOP} path="/shop" component={Shop} />
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   )
--- src/constants/routes.js
+++
@@ -1,4 +0,0 @@
-export const ROUTE_NAMES = {
-  HOMEPAGE: "Homepage",
-  SHOP: "Shop"
-}
